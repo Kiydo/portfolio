adfed7387be96c7193b95ff2db7add2814a9077d
Kiydo <e.magtoto@hotmail.com>
2024-06-02 21:56:09 -0600

    added medias

--- app/_components/projectShowcase/projectCapstone.js
+++ app/_components/projectShowcase/projectCapstone.js
@@ -1,12 +1,14 @@
-import Trailer from '../../media/videos/capstone/trailer.mp4';
+
 import ProjectVideo from '../projectVideo';
 
+
 export default function ProjectCapstone() {
+
     return (
         <div className="min-h-screen bg-gray-100 flex flex-col items-center">
             <h1 className="text-5xl font-bold my-8 text-day-three">My Projects</h1>
             <ProjectVideo
-                videoSrc="/media/videos/capstone/trailer.mp4" // URL relative to the public directory
+                videoSrc="https://www.youtube.com/watch?v=fc3kKh3gbDA" // URL relative to the public directory
                 title="My Awesome Project"
                 description="This is a video showcasing my awesome project. I built this project using React, Node.js, and other cool technologies."
             />
